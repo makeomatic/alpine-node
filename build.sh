c33da3b320720bad52005bdbc6417336636d3576
AVVS <v@makeomatic.ru>
2016-11-08 18:55:54 +0300

    refactor: create all builds & make sure they work

--- build.sh
+++ build.sh
@@ -12,39 +12,45 @@ export \
   PUSH_NAMESPACES=makeomatic \
   BASE_NAME=makeomatic/node
 
+BRANCH_NAME=${BRANCH_NAME:-$(git branch | grep  ^*|cut -d" " -f2)}
+echo "working in $BRANCH_NAME"
+BRANCH_NAME=$(echo $BRANCH_NAME | sed -e "s/\//-/g")
+echo "tagging as $BRANCH_NAME"
+
 # install basic scripts
 curl -sSL https://github.com/makeomatic/ci-scripts/raw/master/install.sh | sh -s
-git clone https://github.com/docker-library/official-images.git ~/official-images
+[ -d ~/official-images ] || git clone https://github.com/docker-library/official-images.git ~/official-images
 
 # add scripts to PATH
 export PATH=$PATH:~/ci-scripts
-[ ${BRANCH_NAME} == master ] || export variant=${BRANCH_NAME}
+
+# cleanup logs
+rm -rf ./*.log
 
 # build base node images that are used further in the project
-docker-build -v "${variant}" $BASE_NAME -f node/Dockerfile .
-docker-build -v "${variant}-onbuild" $BASE_NAME -f node/Dockerfile.onbuild .
-docker-build -v "${variant}-tester" $BASE_NAME -f node/Dockerfile.tester .
-docker-build -v "${variant}-tester-glibc" $BASE_NAME -f node/Dockerfile.tester-glibc .
+docker-build $BASE_NAME -f node/Dockerfile .
+docker-build -v "onbuild" $BASE_NAME -f node/Dockerfile.onbuild .
+docker-build -v "tester" $BASE_NAME -f node/Dockerfile.tester .
+docker-build -v "tester-glibc" $BASE_NAME -f node/Dockerfile.tester-glibc .
 # build node images with ruby
-docker-build -v "${variant}-ruby" $BASE_NAME -f node-ruby/Dockerfile .
+docker-build -v "ruby" $BASE_NAME -f node-ruby/Dockerfile .
 # build node images with ssh embedded
-docker-build -v "${variant}-ssh" $BASE_NAME -f node-ssh/Dockerfile .
-docker-build -v "${variant}-ssh-onbuild" $BASE_NAME -f node-ssh/Dockerfile.onbuild .
+docker-build -v "ssh" $BASE_NAME -f node-ssh/Dockerfile .
+docker-build -v "ssh-onbuild" $BASE_NAME -f node-ssh/Dockerfile.onbuild .
 # build node images with libvips
-docker-build -v "${variant}-vips" $BASE_NAME -f node-vips/Dockerfile .
-docker-build -v "${variant}-vips-onbuild" $BASE_NAME -f node-vips/Dockerfile.onbuild .
-docker-build -v "${variant}-vips-tester-glibc" $BASE_NAME -f node-vips/Dockerfile.tester-glibc .
+docker-build -v "vips" $BASE_NAME -f node-vips/Dockerfile .
+docker-build -v "vips-onbuild" $BASE_NAME -f node-vips/Dockerfile.onbuild .
+docker-build -v "vips-tester-glibc" $BASE_NAME -f node-vips/Dockerfile.tester-glibc .
 # build node images with libvips & ssh
-docker-build -v "${variant}-vips-ssh" $BASE_NAME -f node-vips-ssh/Dockerfile .
-docker-build -v "${variant}-vips-ssh-onbuild" $BASE_NAME -f node-vips-ssh/Dockerfile.onbuild .
+docker-build -v "vips-ssh" $BASE_NAME -f node-vips-ssh/Dockerfile .
+docker-build -v "vips-ssh-onbuild" $BASE_NAME -f node-vips-ssh/Dockerfile.onbuild .
 
 # List of newly created images
-export images=$(docker images | grep "^$BASE_NAME" | tr -s '[:space:]' | cut -f1,2 -d' ' | sed 's/ /:/')
+export images=$(docker images $BASE_NAME | tr -s '[:space:]' | cut -f1,2 -d' ' | sed 's/ /:/')
 
 # we actually need to
 # Push to docker when DEPLOY is true
-# docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-[ ${BRANCH_NAME} == master ] && for image in $images; do docker push $image; done
+[ ${BRANCH_NAME} = master ] && for image in $images; do docker push $image; done
 
 # report
-docker images
+docker images $BASE_NAME
